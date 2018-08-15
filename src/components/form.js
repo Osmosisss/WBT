af3da4165f77544ddfaaac8f254b080b54e71293
Osmosisss <ozyosmosis@gmail.com>
2018-08-14 21:51:31 -0600

    added more styles for the form component

--- src/components/form.js
+++ src/components/form.js
@@ -3,32 +3,39 @@ import React, { Component } from "react";
 class Form extends Component {
   render() {
     return (
-      <form className="form" action="/action_page.php">
-        <div className="form-grid">
-          <div className="form-name">
-            <label for="name">Name:</label>
-            <input type="name" className="form-control" id="name" />
+      <div className="form">
+        <form className="form" action="/action_page.php">
+          <div className="form-grid">
+            <div className="form-name">
+              <label for="name">Name</label>
+              <input type="name" className="form-control" id="name" />
+            </div>
+            <div className="form-email">
+              <label for="email">Email address</label>
+              <input type="email" className="form-control" id="email" />
+            </div>
+            <div className="form-phone">
+              <label for="pwd">Phone</label>
+              <input type="phone" className="form-control" id="phone" />
+            </div>
+            <div className="form-comp">
+              <label for="pwd">Company Name (Optional)</label>
+              <input type="comp" className="form-control" id="comp" />
+            </div>
+            <div className="form-message">
+              <textarea
+                className="form-control"
+                name="message"
+                rows="4"
+                cols="90"
+              />
+            </div>
+            <button type="submit" className="btn btn-default">
+              Send
+            </button>
           </div>
-          <div className="form-email">
-            <label for="email">Email address:</label>
-            <input type="email" className="form-control" id="email" />
-          </div>
-          <div className="form-phone">
-            <label for="pwd">Phone:</label>
-            <input type="phone" className="form-control" id="phone" />
-          </div>
-          <div className="form-comp">
-            <label for="pwd">Company Name:</label>
-            <input type="comp" className="form-control" id="comp" />
-          </div>
-          <div className="form-message">
-            <textarea name="message" rows="10" cols="90" />
-          </div>
-          <button type="submit" className="btn btn-default">
-            Send
-          </button>
-        </div>
-      </form>
+        </form>
+      </div>
     );
   }
 }
