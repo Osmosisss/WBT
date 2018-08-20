ec35d1a41f31a60a294db538634dce70fa0cbeae
Your Name <christian.cosio1@gmail.com>
2018-08-19 22:16:13 -0700

    new-mobile-slider

---
+++ src/components/mobile-slider.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MobileSlider extends Component {
+    render() {
+        return (
+            <div>
+              this is the form for the new mobile slider   
+            </div>
+        );
+    }
+}
+
+export default MobileSlider;
